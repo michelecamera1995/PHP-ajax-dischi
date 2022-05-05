be2314ad4d0ed7aae3f9e53455ef238dc9f1bd8c
michele camera <michy.cam95@gmail.com>
2022-05-05 15:10:41 +0200

    creation first exercise

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+include 'data.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Dischi</title>
+</head>
+
+<body>
+    <div id="main">
+        <div class="top-bar">
+            <img class="spotify-logo" src="" alt="spotify logo" />
+        </div>
+        <div id="mainbottom">
+            <?php
+            foreach ($musiclist as $music) {
+                $poster = ('<img class="ms-img" src="' . $music['poster'] . '" alt="music poster" />');
+                $genre = ('<h3>' . $music['genre']  . '</h3>');
+                $author = ('<h4>' . $music['author']  . '</h4>');
+                echo ('<div class="ms-card"' . $poster . $genre . $author . '</div>');
+            }
+            ?>
+        </div>
+    </div>
+</body>
+
+</html>
